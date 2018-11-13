668b009618ec5a0b178352a1ee30a718b051b8fc
Grupa 2 Java <alexandra.dumitru1218@gmail.com>
2018-11-13 20:57:50 +0200

    Initial commit

---
+++ src/Robot.java
@@ -0,0 +1,20 @@
+import stanford.karel.Karel;
+
+         public class Robot extends Karel {
+             public void run() {
+                 turnLeft();
+                 while (frontIsClear()) {
+                     if (noBeepersPresent()) {
+                         putBeeper();
+                     }
+                     move();
+                 }
+                     if (noBeepersPresent()) {
+                         putBeeper();
+
+                     }
+
+                 }
+
+             }
+
